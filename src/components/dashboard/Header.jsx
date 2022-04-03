92d9a3ac63e14cc923a41fc990cedc7797871547
Peanuts-83 <tranque@free.fr>
2022-04-03 08:56:48 +0200

    vertical nav responsive fix

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -9,7 +9,7 @@ function Header({ firstname }) {
 	return (
 		<div className="Header">
 			<h1>Bonjour <span className="name" data-testid="firstname">{firstname}</span></h1>
-			<p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+			<p>Félicitations! Vous avez explosé vos objectifs hier 👏</p>
 		</div>
 	);
 }
